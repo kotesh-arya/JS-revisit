fcc4dbdc415597182e442c344832c4be8a77c124
kotesh-arya <koteswarraomudila@gmail.com>
2024-01-13 22:41:36 +0530

    ADD: Decorator function and challenge-1

---
+++ Decorator-functions/index.js
@@ -0,0 +1,75 @@
+function slow(x) {
+  // there can be a heavy CPU-intensive job here
+  console.log(`Called with ${x}`);
+  return x;
+}
+
+function cachingDecorator(func) {
+  let cache = new Map();
+
+  return function (x) {
+    if (cache.has(x)) {
+      // if there's such key in cache
+      return cache.get(x); // read the result from it
+    }
+
+    let result = func(x); // otherwise call func
+    console.log(func);
+    cache.set(x, result); // and cache (remember) the result
+    return result;
+  };
+}
+
+slow = cachingDecorator(slow);
+
+console.log(slow);
+slow(3);
+
+// console.log(slow(1)); // slow(1) is cached and the result returned
+// console.log("Again: " + slow(1)); // slow(1) result returned from cache
+
+// console.log(slow(2)); // slow(2) is cached and the result returned
+// console.log("Again: " + slow(2));
+
+// --------------------------------------------------
+// Challenges
+// Challenge 1
+
+// Spy decorator
+// importance: 5
+// Create a decorator spy(func) that should return a wrapper that saves all calls to function in its calls property.
+
+// Every call is saved as an array of arguments.
+
+// For instance:
+
+//  work.calls = [1,2]
+
+// function argToString(args) {
+//   return args[0] + args[1];
+// }
+function work(a, b) {
+  console.log(a + b); // work is an arbitrary function or method
+}
+// console.log(work.calls, "hihi");
+
+function spy(workFunc) {
+  return function () {
+    if (work.calls.includes(arguments)) {
+      return;
+    }
+    work.calls.push(Array.from(arguments));
+    let result = workFunc(...arguments);
+    return result;
+  };
+}
+work = spy(work);
+work.calls = []; //Setting a property to the work function with new body
+
+work(1, 2); // 3
+work(4, 5); // 9
+work(41, 1);
+console.log(work.calls, "at lasttt");
+for (let args of work.calls) {
+  console.log("call:" + args.join()); // "call:1,2", "call:4,5"
+}
