c7d10a3349044955bc7f578c1815b2b69ea2b0fc
kotesh-arya <koteswarraomudila@gmail.com>
2024-01-14 19:18:16 +0530

    ADD: Call forwarding and Method Borrowing

--- Decorator-functions/index.js
+++ Decorator-functions/index.js
@@ -31,6 +31,48 @@ slow(3);
 // console.log(slow(2)); // slow(2) is cached and the result returned
 // console.log("Again: " + slow(2));
 
+// Call forwarding
+
+// The phenomenon of passing/forwarding the CONTEXT & ARGUMENTS of parent function  when calling the callback function inside the parent function is called Call Forwarding
+
+function child(referencingObj) {
+  console.log(
+    `I am a child function being called and I have the`,
+    arguments,
+    `of the parent function`
+  );
+}
+let funcFamObj = {
+  parent(childCb) {
+    childCb(this, arguments);
+  },
+};
+funcFamObj.parent(child);
+
+// Method Borrowing from an Object
+// Ex: Using Join method of Array
+console.log(Array.prototype, "***");
+
+let arrayLike = {
+  0: "apple",
+  1: "banana",
+  2: "orange",
+  length: 3, // It's important to set the length property
+};
+
+function getArgs() {
+  console.log(
+    [].join.call(arrayLike),
+    "elements of an array-like joined by a comma here"
+  );
+  console.log(
+    [].join.call(arguments),
+    "arguments of the function joined by a comma here"
+  );
+}
+getArgs(1, 2, 3, 4);
+// getArgs();
+
 // --------------------------------------------------
 // Challenges
 // Challenge 1
@@ -113,8 +155,8 @@ function debounce(func, debounceDelay) {
 
 debouncedFunc = debounce(f1, 4000);
 
-const userInput = document.getElementById("user-input");
+// const userInput = document.getElementById("user-input");
 
-userInput.addEventListener("input", (e) => {
-  debouncedFunc(e.target.value);
-});
+// userInput.addEventListener("input", (e) => {
+//   debouncedFunc(e.target.value);
+// });
