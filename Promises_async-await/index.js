e134068cd3bd2f5ffded0cfbb3b6b4bc37c460bb
kotesh-arya <koteswarraomudila@gmail.com>
2024-02-13 20:51:23 +0530

    ADD: asynchronous callback functions

---
+++ Promises_async-await/index.js
@@ -0,0 +1,13 @@
+// calling a callback function inside a higher-order function with a delay and make it asynchronous
+
+
+function sample() {
+    console.log("from an async function")
+}
+function parent(cb) {
+    setTimeout(cb, 1000)
+}
+
+parent(sample);
+
+// the cb function is initiated now but it will be finished later
