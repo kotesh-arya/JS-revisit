d1499fd289cf372563a1c94a19cfe68616f2e9ec
kotesh-arya <koteswarraomudila@gmail.com>
2024-02-17 21:47:40 +0530

    ADD: Pyramid of doom alias Callback hell

--- Promises_async-await/index.js
+++ Promises_async-await/index.js
@@ -2,10 +2,10 @@
 
 
 function sample() {
-    console.log("from an async function")
+  console.log("from an async function")
 }
 function parent(cb) {
-    setTimeout(cb, 1000)
+  setTimeout(cb, 1000)
 }
 
 parent(sample);
@@ -13,13 +13,13 @@ parent(sample);
 // the cb function is initiated now but it will be finished later
 
 
-function generateScript(src){
-    let scriptElement = document.createElement("script");
-    scriptElement.src = src;
-    document.head.append(scriptElement);
-}
+// function generateScript(src) {
+//   let scriptElement = document.createElement("script");
+//   scriptElement.src = src;
+//   document.head.append(scriptElement);
+// }
 
-generateScript("./sample.js");
+// generateScript("./sample.js");
 
 
 
@@ -47,27 +47,59 @@ generateScript("./sample.js");
 
 // Let's put the second loadScript call inside the callback, like this:
 
-loadScript('/my/script.js', function(script) {
+// loadScript('/my/script.js', function (script) {
 
-    alert(`Cool, the ${script.src} is loaded, let's load one more`);
-  
-    loadScript('/my/script2.js', function(script) {
-      alert(`Cool, the second script is loaded`);
-    });
-  
-  });
-  
-  
-  // What if we want one more script…?
-  
-  loadScript('/my/script.js', function(script) {
-  
-    loadScript('/my/script2.js', function(script) {
-  
-      loadScript('/my/script3.js', function(script) {
-        // ...continue after all scripts are loaded
+//   alert(`Cool, the ${script.src} is loaded, let's load one more`);
+
+//   loadScript('/my/script2.js', function (script) {
+//     alert(`Cool, the second script is loaded`);
+//   });
+
+// });
+
+
+// What if we want one more script…?
+
+// loadScript('/my/script.js', function (script) {
+
+//   loadScript('/my/script2.js', function (script) {
+
+//     loadScript('/my/script3.js', function (script) {
+//       // ...continue after all scripts are loaded
+//     });
+
+//   });
+
+// });
+
+
+// Pyramid of Dooom using Simulated asynchronous functions with callbacks
+function asyncFunction1(callback) {
+  setTimeout(() => {
+      const result1 = 'Result from asyncFunction1';
+      callback(result1);
+  }, 1000);
+}
+
+function asyncFunction2(data, callback) {
+  setTimeout(() => {
+      const result2 = 'Result from asyncFunction2 using ' + data;
+      callback(result2);
+  }, 1000);
+}
+
+function asyncFunction3(data, callback) {
+  setTimeout(() => {
+      const result3 = 'Result from asyncFunction3 using ' + data;
+      callback(result3);
+  }, 1000);
+}
+
+// Nested callbacks forming the "pyramid of doom"
+asyncFunction1(function(result1) {
+  asyncFunction2(result1, function(result2) {
+      asyncFunction3(result2, function(result3) {
+          console.log(result3); // Output the final result
       });
-  
-    });
-  
   });
+});
