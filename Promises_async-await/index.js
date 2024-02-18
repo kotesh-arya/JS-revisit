fc051b325db4f6c2da1b0fec1c9a74a90206ccc2
kotesh-arya <koteswarraomudila@gmail.com>
2024-02-18 22:19:17 +0530

    ADD: Blocking the main thread by executing a sluggish function on the main thread

--- Promises_async-await/index.js
+++ Promises_async-await/index.js
@@ -1,17 +1,15 @@
 // calling a callback function inside a higher-order function with a delay and make it asynchronous
 
-
 function sample() {
-  console.log("from an async function")
+  console.log("from an async function");
 }
 function parent(cb) {
-  setTimeout(cb, 1000)
+  setTimeout(cb, 1000);
 }
 
 parent(sample);
 
 // the cb function is initiated now but it will be finished later
-
 
 // function generateScript(src) {
 //   let scriptElement = document.createElement("script");
@@ -21,8 +19,6 @@ parent(sample);
 
 // generateScript("./sample.js");
 
-
-
 // function loadScript(src, callback) {
 //     let script = document.createElement('script');
 //     script.src = src;
@@ -30,20 +26,15 @@ parent(sample);
 //     document.head.append(script);
 //   }
 
-
-
 //   loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
 //   console.log(`Cool, the script ${script.src} is loaded`); -->  "Cool, the script https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js is loaded"
 //   console.log( _ ); // _ is a function declared in the loaded script
 // });
 
-
 // It’s called a “callback-based” style of asynchronous programming.
 // A function that does something asynchronously should provide a callback argument where we put the function to run after it’s complete.
 
-
 // Callback in callback
-
 
 // Let's put the second loadScript call inside the callback, like this:
 
@@ -56,7 +47,6 @@ parent(sample);
 //   });
 
 // });
-
 
 // What if we want one more script…?
 
@@ -72,34 +62,45 @@ parent(sample);
 
 // });
 
-
 // Pyramid of Dooom using Simulated asynchronous functions with callbacks
 function asyncFunction1(callback) {
   setTimeout(() => {
-      const result1 = 'Result from asyncFunction1';
-      callback(result1);
+    const result1 = "Result from asyncFunction1";
+    callback(result1);
   }, 1000);
 }
 
 function asyncFunction2(data, callback) {
   setTimeout(() => {
-      const result2 = 'Result from asyncFunction2 using ' + data;
-      callback(result2);
+    const result2 = "Result from asyncFunction2 using " + data;
+    callback(result2);
   }, 1000);
 }
 
 function asyncFunction3(data, callback) {
   setTimeout(() => {
-      const result3 = 'Result from asyncFunction3 using ' + data;
-      callback(result3);
+    const result3 = "Result from asyncFunction3 using " + data;
+    callback(result3);
   }, 1000);
 }
 
 // Nested callbacks forming the "pyramid of doom"
-asyncFunction1(function(result1) {
-  asyncFunction2(result1, function(result2) {
-      asyncFunction3(result2, function(result3) {
-          console.log(result3); // Output the final result
-      });
+asyncFunction1(function (result1) {
+  asyncFunction2(result1, function (result2) {
+    asyncFunction3(result2, function (result3) {
+      console.log(result3); // Output the final result
+    });
   });
 });
+
+
+// Blocking the main thread
+function timeTakingFunc() {
+  for (let index = 0; index < 1000000000; index++) {
+    const element = index;
+    console.log(element);
+  }
+}
+timeTakingFunc();
+
+
