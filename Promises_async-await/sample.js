2b22622e1e885ac59442e54c7599353b2fbbf985
kotesh-arya <koteswarraomudila@gmail.com>
2024-03-04 20:22:28 +0530

    ADD: Task on promise chaining

--- Promises_async-await/sample.js
+++ Promises_async-await/sample.js
@@ -31,3 +31,47 @@ fetch("https://jsonplaceholder.typicode.com/todos/1")
   });
 
 //   NOTE: Its always a GOOD PRACTICE to return promise from a .then()'s [or a catch() or a finally()] handler function, so that the next .then()'s [or a catch() or a finally()] handler will wait till the above promise is resolved and that makes delayed asynchronous operations more ordered and smooth.
+
+
+// TASKS:
+
+
+// Promise: then versus catch
+// Are these code fragments equal? In other words, do they behave the same way in any circumstances, for any handler functions?
+
+// promise.then(f1).catch(f2);
+
+// Versus:
+
+// promise.then(f1, f2);
+
+const myPromise = new Promise((resolve, reject) => {
+  // uncomment either of these
+  resolve("SUCCESS");
+  // reject(new Error("Sorry you're failed!"));
+});
+
+// Case 1
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Case 2
+// myPromise.then(
+//   (result) => {
+//     console.log(result);
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+
+// Solution: Here in the first case if there's an error occured in either resolving the promise or from the .then() block the .catch() can handle both of the cases
+
+//  But in the second case of passing two handler functions into the .then() the second habdler function of course will execute in case of an error while executing the promise 
+// BUT IT IS NOT CAPABLE OF HANDLE AN ERROR THAT IS OCCURED IN THE FIRST HANDLER FUNCTION OF THE .then()
